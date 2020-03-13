c2740bfd61afb61a321c24a721b62623e977caa8
sylhuynh <60867374+sylhuynh@users.noreply.github.com>
2020-03-12 20:33:51 -0700

    created an array for each character user can choose to include in password

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// let lowerCaseChars = all the lowercase characters
+// Assignment Code
+var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// let upperCaseChars = all the uppercase characters
+var upperCaseChars = ["A" , "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+// let numberChars = all the number characters
+var numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// let special = all the allowed special characters
+var specialChars = ["!", "#", "$", "%", "&", "'", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{" ,"|", "}", "~"]; 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
